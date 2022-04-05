27989db5e33799cc781782696e171d4cd11841e6
David Wampamba <davidwampamba@gmail.com>
2022-04-05 15:31:22 +0300

    Form without Formik

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Logo from "./shared/Logo";
 const Login = () => {
-    return ( <
-        div >
-        <
-        Logo width = "32"
+    const [email, setEmail] = React.useState("");
+    const [error, setError] = React.useState("");
+    return (<div>
+        <Logo width = "32"
         height = "32"
-        title = "Group Fund Manager" / > { " " } <
-        /div>
+        title = "Group Fund Manager" /> { " " } <
+        input className = { `p-2 border ${error ? "border-red-500" : "border-gray-200"}` }
+        type="text"
+        placeholder="Email/Phone"
+        onChange={(event) => setEmail(event.target.value) }
+        onBlur={(event) => event.target.value === "" ? setError("Email required!") : null}
+        onFocus={() => setError("")}
+        />{" "}
+        {error && <div className="text-red-500">{error}</div>}
+        The email is { email } { " " }
+        
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import *
+as ReactDOMClient from 'react-dom/client';
+
 import "./index.css";
 import App from "./App";
 import Login from "./components/Login";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+root.render( <
+    React.StrictMode >
+    <
+    Login / >
+    <
+    /React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+reportWebVitals();
 reportWebVitals();

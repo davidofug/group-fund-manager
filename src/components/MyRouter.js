f3bc2b329afece4dddec3955a71093b1ee54547b
David Wampamba <davidwampamba@gmail.com>
2022-04-07 19:29:48 +0300

    Router bug fix

--- src/components/MyRouter.js
+++ src/components/MyRouter.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import ForgotPassword from "./ForgotPassword";
+import Layout from "./shared/Layout";
 
 const MyRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/" element={<Layout />} >
+                    <Route path="/" element={<Login />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                </Route>
             </Routes>
         </Router>
     )
---
+++ src/components/shared/Layout.js
@@ -0,0 +1,9 @@
+import {Outlet} from 'react-router-dom'
+const Layout = () => {
+    return (
+        <>
+            <Outlet />
+        </>
+    )
+    }
+export default Layout
